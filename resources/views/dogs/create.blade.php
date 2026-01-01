8042faedcba59777d9a16c3bb775be1f207207ee
tomo-john <keririnkery.kery@gmail.com>
2026-01-01 13:08:35 +0900

    🐰 refactor: フォームのパーシャル化とデザイン変更

--- resources/views/dogs/create.blade.php
+++ resources/views/dogs/create.blade.php
@@ -5,54 +5,12 @@
         </h2>
     </x-slot>
 
-    <!-- バリデーションエラー -->
-    @if ($errors->any())
-        <div class="mb-4 p-3 bg-red-100 text-red-800 rounded">
-            <ul class="list-disc list-inside">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="max-w-2xl mx-auto px-6 py-8">
+        <div class="bg-white rounded-2xl shadow-md p-6">
+            <form method="POST" action="{{ route('dogs.store') }}" class="space-y-5">
+                @csrf
+                @include('dogs._form', ['submitText' => '登録'])
+            </form>
         </div>
-    @endif
-
-    <div class="p-6">
-        <form method="POST" action="{{ route('dogs.store') }}">
-            @csrf
-
-            <div>
-                <label>名前</label>
-                <input name="name" class="border" value="{{ old('name', $dog->name )}}">
-            </div>
-
-            <div>
-                <label>毛色</label>
-                <input name="color" class="border" value="{{ old('color', $dog->color) }}">
-            </div>
-
-            <div>
-                <label>サイズ</label>
-                <select name="size" class="border">
-                    @foreach($sizes as $value => $label)
-                        <option value="{{ $value }}"
-                            @selected(old('size', $dog->size) === $value)>
-                            {{ $label }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div>
-                <label>
-                    <input type="checkbox" name="is_public" value="1"
-                        @checked(old('is_public', $dog->is_public))>
-                    公開する
-                </label>
-            </div>
-
-            <button class="mt-4 bg-blue-500 text-white px-4 py-2">
-                登録
-            </button>
-        </form>
     </div>
 </x-app-layout>
