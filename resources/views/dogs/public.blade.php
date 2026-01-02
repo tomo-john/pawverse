4bc140658e1cf82f5c878661e809a9f4cd3f30bc
tomo-john <keririnkery.kery@gmail.com>
2026-01-02 09:15:18 +0900

    🐰 feat: 飼い主名の表示を追加

--- resources/views/dogs/public.blade.php
+++ resources/views/dogs/public.blade.php
@@ -8,15 +8,24 @@
     <div class="p-6">
         <ul>
             @forelse ($dogs as $dog)
-                <li class="border rounded-xl p-4 mb-4 bg-white shadow-sm">
-                    <a href="{{ route('dogs.show', $dog) }}"
-                       class="font-semibold text-lg hover:text-pink-500">
-                        {{ $dog->name }}
-                    </a>
+                <li class="flex justify-between border rounded-xl p-4 mb-4 bg-white shadow-sm">
+                    <div>
+                        <a href="{{ route('dogs.show', $dog) }}"
+                           class="font-semibold text-lg hover:text-pink-500">
+                            {{ $dog->name }}
+                        </a>
 
-                    <div class="text-sm text-gray-500">
-                        {{ $dog->size }}
-                        <span class="ml-2 text-green-600">🌍 公開中</span>
+                        <div class="text-sm text-gray-500">
+                            {{ $dog->size }}
+                            <span class="ml-2 text-green-600"><i class="fa-solid fa-palette text-yellow-500 mr-1"></i>公開中</span>
+                        </div>
+                    </div>
+
+                    <div class="self-center">
+                        <span class="text-sm text-gray-500 mr-2">飼い主: </span>
+                        <button class="font-semibold text-md bg-yellow-500 hover:bg-yellow-600 rounded-full px-4 py-2">
+                            {{ $dog->user['name'] }}
+                        </button>
                     </div>
                 </li>
             @empty
