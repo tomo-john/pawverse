36ce11019e7673502528505d9af436a289d88f48
tomo-john <keririnkery.kery@gmail.com>
2026-01-17 15:45:52 +0900

    🐶 test: User用Seederを作成

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // 管理者ユーザー
+        User::Create([
+            'name' => 'john',
+            'email' => 'john@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'adimn',
+        ]);
+
+        // 一般ユーザー
+        User::Create([
+            'name' => 'pyon',
+            'email' => 'pyon@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]);
+    }
+}
