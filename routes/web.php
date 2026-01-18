024401f5b4ccbf36489f1c941802645190e71f53
tomo-john <keririnkery.kery@gmail.com>
2026-01-18 13:37:53 +0900

    🐰 feat: adminミドルウェア作成とルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])
+        ->name('admin.dashboard');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
