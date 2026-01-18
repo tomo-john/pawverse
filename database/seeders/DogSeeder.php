4f2ce94ab3c11ea2ed07fbff13b0b030e8003bdf
tomo-john <keririnkery.kery@gmail.com>
2026-01-18 15:24:36 +0900

    🐶 test: adminにもdog作成を追加

--- database/seeders/DogSeeder.php
+++ database/seeders/DogSeeder.php
@@ -11,6 +11,16 @@ class DogSeeder extends Seeder
 {
     public function run(): void
     {
+        // 管理者ユーザーに犬を持たせる
+        $admin = User::where('role', 'admin')->first();
+        if ($admin) {
+            Dog::factory()
+                ->count(6)
+                ->create([
+                    'user_id' => $admin->id,
+                ]);
+        }
+
         // 一般ユーザーに犬を持たせる
         User::where('role', 'user')->each(function ($user) {
             Dog::factory()
