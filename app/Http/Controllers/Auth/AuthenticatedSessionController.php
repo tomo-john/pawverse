8a344f9c80b1852ac64a3c329ffb40b5567ad0b7
tomo-john <keririnkery.kery@gmail.com>
2026-01-18 15:02:52 +0900

    🐰 refacotr: adminのログイン後画面変更

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (auth()->user()->isAdmin()) {
+            return redirect()->route('admin.dashboard');
+        }
+
         return redirect()->intended(route('dashboard'));
     }
 
