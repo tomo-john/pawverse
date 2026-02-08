697405039b43a20e45b9dee89e9d679786dff3c4
tomo-john <keririnkery.kery@gmail.com>
2026-02-08 17:47:53 +0900

    🐶 chore: Alpine確認
    - Alpineの修行へようこそ
    - 最小構成で動かしてみる

--- resources/views/components/sandbox/dog.blade.php
+++ resources/views/components/sandbox/dog.blade.php
@@ -1,4 +1,11 @@
-<div>
-    呼び出された犬
-    <i class="fa-solid fa-dog text-pink-500"></i>
+<div
+    x-data="{
+        message: '呼び出された犬'
+    }"
+>
+    <span x-text="message"></span>
+
+    <i class="fa-solid fa-dog text-pink-500 cursor-pointer"
+       x-on:click="message = 'わんわん！🐾'">
+    </i>
 </div>
