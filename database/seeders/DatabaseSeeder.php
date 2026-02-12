b32d1ea61c23b03694aaebb07dbdedb3644851e5
tomo-john <keririnkery.kery@gmail.com>
2026-02-13 08:04:06 +0900

    🐶 chore: User作成用のseeder作成
    - UserFactoryにis_admin追加
    - seederにて固定ユーザーを3人
    - Admin: 1人、User: 2人

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // 固定の管理者ユーザー (Admin john)
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin john',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
         ]);
+
+        // 固定の一般ユーザー (User john)
+        User::factory()->create([
+            'name' => 'User john',
+            'email' => 'john@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // 固定の一般ユーザー (User pyon)
+        User::factory()->create([
+            'name' => 'User pyon',
+            'email' => 'pyon@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // ランダムユーザーの作成
+        // User::factory(10)->create();
     }
 }
