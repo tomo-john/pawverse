23efb53bd6240a191575418ec45ecea75dda9b3c
tomo-john <keririnkery.kery@gmail.com>
2026-03-01 15:51:09 +0900

    🐰 feat: Dog/LogFormのLivewire作成(のみ)
    - 中身これから

---
+++ app/Livewire/Dog/LogForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dog;
+
+use Livewire\Component;
+
+class LogForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.dog.log-form');
+    }
+}
