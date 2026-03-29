96217336bb960b0abb859a727ebddc20c334114e
tomo-john <keririnkery.kery@gmail.com>
2026-03-30 07:23:12 +0900

    🐶 chore: マウス追従犬
    - isMoving = Math.abs(y - $event.clientY) > 2;
    - 前の位置との差分を見る

--- resources/views/livewire/dog/show.blade.php
+++ resources/views/livewire/dog/show.blade.php
@@ -2,16 +2,19 @@
 
     {{-- 左側: 犬専用レーン --}}
     <div class="w-32 flex justify-center"
-         x-data="{y: 0}"
-         @mousemove.window="y = $event.clientY"
+         x-data="{ y: 0, isMoving: false }"
+         @mousemove.window="
+            isMoving = Math.abs(y - $event.clientY) > 2;
+            y = $event.clientY
+        "
     >
             {{-- Dog --}}
             <div class="relative w-24 h-24 flex justify-center items-center"
                  wire:poll.1s="checkAnimation"
                  :style="`position: fixed; top: ${y}px; transform: translateY(-50%); transition: top 0.5s cubic-bezier(0.22, 1, 0.36, 1)`"
             >
-                <i class="fa-solid fa-dog {{ $dog->size_class }} hover:scale-110 transition
-                          {{ $this->animationClass }}"
+                <i class="fa-solid fa-dog {{ $dog->size_class }} hover:scale-110 transition {{ $this->animationClass }}"
+                   :class="{ 'animate-spin' : isMoving }"
                    style="color: {{ $dog->color }}"
                 ></i>
             </div>
