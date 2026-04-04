d22b7621f02d90418ef3b8c15df4320378869112
tomo-john <keririnkery.kery@gmail.com>
2026-04-04 14:52:03 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/alpine/alpine6.blade.php
+++ resources/views/components/sandbox/alpine/alpine6.blade.php
@@ -2,21 +2,36 @@
 <div x-data="{
         x: 10,
         y: 10,
+        green: false,
 
         move($event) {
             this.x = $event.offsetX;
             this.y = $event.offsetY;
         },
+
+        reset() {
+            this.x = 10;
+            this.y = 10;
+        },
+
+        changeGreen() {
+            this.green = ! this.green;
+        },
     }"
 >
 
-    <div class="h-96 m-6 bg-white rounded-xl shadow border border-gray-700 relative cursor-crosshair"
+    <div class="h-96 m-6 rounded-xl shadow border border-gray-700 relative cursor-crosshair"
+         :class="{ 'bg-green-200': green, 'bg-white': ! green }"
          @click="move($event)"
+         @keydown.window.r="reset()"
+         @keydown.window.shift.r="changeGreen()"
     >
         <div class="absolute transition-all duration-1000 ease-in-out"
              :style="`left: ${x}px; top: ${y}px;`"
         >
-            <i class="fa-solid fa-dog text-3xl text-pink-500"></i>
+            <i class="fa-solid fa-dog text-3xl"
+                 :class="{ 'text-green-600': green, 'text-black': ! green }"
+            ></i>
         </div>
     </div>
 
