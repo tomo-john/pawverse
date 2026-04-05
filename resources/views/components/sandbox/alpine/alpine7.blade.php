af5bd5db5790e54f6f499974e9e9a729d93a6651
tomo-john <keririnkery.kery@gmail.com>
2026-04-05 15:33:43 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/alpine/alpine7.blade.php
+++ resources/views/components/sandbox/alpine/alpine7.blade.php
@@ -2,21 +2,36 @@
 <div x-data="{
         x: 0,
         y: 0,
+        maxX: 0,
+        maxY: 0,
         timer: null,
         isLeft: false,
 
         init() {
+            this.maxX = this.$refs.field.clientWidth / 2;
+            this.maxY = this.$refs.field.clientHeight / 2;
+
             this.walk();
         },
 
         walk() {
             this.timer = setInterval(() => {
-                distance = this.random();
+
+                let distance = this.random();
                 this.isLeft = distance < 0 ? true : false;
-                this.x += distance;
+
+                let newX = this.x + distance;
+                if ( newX > -this.maxX && newX < this.maxX ) {
+                    this.x = newX;
+                };
 
                 distance = this.random();
-                this.y += distance;
+
+                let newY = this.y + distance;
+                if ( newY > -this.maxY && newY < this.maxY ) {
+                    this.y = newY;
+                };
+
             }, 1000);
         },
 
@@ -26,7 +41,7 @@
     }"
 >
 
-    <div class="h-96 m-6 bg-white rounded-xl shadow border border-gray-700 flex justify-center items-center">
+    <div class="h-96 m-6 bg-white rounded-xl shadow border border-gray-700 flex justify-center items-center" x-ref="field">
         <div class="relative">
             <div class="absolute transition-all duration-1000 ease-linear" :style="{ left: x + 'px', top: y + 'px'}">
                 <i class="fa-solid fa-dog text-sky-300 text-3xl"
