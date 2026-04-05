51354289dd6296824b34d753e05d8ae9f8dc9155
tomo-john <keririnkery.kery@gmail.com>
2026-04-05 14:42:23 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/alpine/alpine7.blade.php
+++ resources/views/components/sandbox/alpine/alpine7.blade.php
@@ -2,13 +2,36 @@
 <div x-data="{
         x: 0,
         y: 0,
+        timer: null,
+        isLeft: false,
+
+        init() {
+            this.walk();
+        },
+
+        walk() {
+            this.timer = setInterval(() => {
+                distance = this.random();
+                this.isLeft = distance < 0 ? true : false;
+                this.x += distance;
+
+                distance = this.random();
+                this.y += distance;
+            }, 1000);
+        },
+
+        random() {
+            return Math.floor(Math.random() * 101) - 50;
+        },
     }"
 >
 
     <div class="h-96 m-6 bg-white rounded-xl shadow border border-gray-700 flex justify-center items-center">
         <div class="relative">
-            <div class="absolute" :style="{ left: x + 'px', top: y + 'px'}">
-                <i class="fa-solid fa-dog text-sky-300 text-3xl"></i>
+            <div class="absolute transition-all duration-1000 ease-linear" :style="{ left: x + 'px', top: y + 'px'}">
+                <i class="fa-solid fa-dog text-sky-300 text-3xl"
+                   :class="{ '-scale-x-100': isLeft }"
+                ></i>
             </div>
         </div>
     </div>
