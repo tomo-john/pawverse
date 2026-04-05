83f51a2a9c2f51011c16fd383b3e7b5914ad422a
tomo-john <keririnkery.kery@gmail.com>
2026-04-06 08:10:05 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/alpine/alpine9.blade.php
+++ resources/views/components/sandbox/alpine/alpine9.blade.php
@@ -3,13 +3,13 @@
     <div class="m-6 p-6 bg-white rounded-xl shadow border border-gray-700 flex flex-col items-center gap-3">
 
         <div class="bg-green-200 rounded-lg w-full p-6 flex items-center relative">
-            <div class="transition-all duration-1000 absolute" :style="{ left: x + 'px'}">
+            <div class="transition-all duration-1000 absolute" :style="{ left: x1 + 'px'}">
                 <i class="fa-solid fa-dog text-green-500 text-3xl"></i>
             </div>
         </div>
 
         <div class="bg-red-200 rounded-lg w-full p-6 flex items-center relative">
-            <div class="transition-all duration-1000 absolute" :style="{ left: x + 'px'}">
+            <div class="absolute" :style="{ left: x2 + 'px'}">
                 <i class="fa-solid fa-dog text-red-500 text-3xl"></i>
             </div>
         </div>
@@ -21,17 +21,24 @@
 <script>
     function animate() {
         return {
-            x: 0,
+            x1: 0,
+            x2: 0,
             timer: null,
 
             init() {
                 this.walk();
+                this.walkRed();
             },
 
             walk() {
-                timer = setInterval(() => {
-                    this.x += 10;
+                this.timer = setInterval(() => {
+                    this.x1 += 10;
                 }, 1000);
+            },
+
+            walkRed() {
+                this.x2 += 0.166;
+                requestAnimationFrame(() => this.walkRed());
             },
         }
     }
