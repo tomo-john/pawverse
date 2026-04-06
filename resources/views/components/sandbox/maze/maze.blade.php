bd7d0f97bfc3d7ae0778d2f8f30cdd947469b423
tomo-john <keririnkery.kery@gmail.com>
2026-04-07 08:33:33 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/maze/maze.blade.php
+++ resources/views/components/sandbox/maze/maze.blade.php
@@ -3,18 +3,25 @@
 </div>
 
 <div x-data="maze"
-     @keydown.window.prevent="handleKey($event)"
+     @keydown.window.arrow-up.prevent="handleKey($event)"
+     @keydown.window.arrow-down.prevent="handleKey($event)"
+     @keydown.window.arrow-left.prevent="handleKey($event)"
+     @keydown.window.arrow-right.prevent="handleKey($event)"
 >
 
-    <div class="w-full h-96 border rounded-lg relative">
-
-        <div class="absolute"
+    <div class="w-full h-96 border rounded-lg overflow-hidden relative" x-ref="field">
+        <div class="absolute transition-all duration-100"
              :style="{left: x + 'px', top: y + 'px'}"
         >
-            <i class="fa-solid fa-dog text-white"></i>
-
+            <i class="fa-solid fa-dog text-white"
+               :class="isLeft ? '-scale-x-100' : ''"
+            ></i>
         </div>
+    </div>
 
+    <div class="my-4">
+        <span class="text-sm text-gray-400" x-text="`x: ${x}px / y: ${y}px`"></span>
+    </div>
 </div>
 
 <script>
@@ -23,24 +30,39 @@
         return {
             x: 0,
             y: 0,
+            maxX: 0,
+            maxY: 0,
+            isLeft: false,
+
+            init() {
+                this.maxX = this.$refs.field.clientWidth - 30;
+                this.maxY = this.$refs.field.clientHeight - 30;
+            },
+
+            clamp(val, min, max) {
+                return Math.min(Math.max(val, min), max);
+            },
 
             handleKey(event) {
-                console.log('押されたキーは:', event.key);
+                const step = 10;
+                switch (event.key) {
+                    case 'ArrowUp':
+                        this.y = this.clamp(this.y - step, 0, this.maxY);
+                        break;
 
-                if (event.key === 'ArrowUp') {
-                    this.y -= 10;
-                }
+                    case 'ArrowDown':
+                        this.y = this.clamp(this.y + step, 0, this.maxY);
+                        break;
 
-                if (event.key === 'ArrowDown') {
-                    this.y += 10;
-                }
+                    case 'ArrowLeft':
+                        this.isLeft = true;
+                        this.x = this.clamp(this.x - step, 0, this.maxX);
+                        break;
 
-                if (event.key === 'ArrowLeft') {
-                    this.x -= 10;
-                }
-
-                if (event.key === 'ArrowRight') {
-                    this.x += 10;
+                    case 'ArrowRight':
+                        this.isLeft = false;
+                        this.x = this.clamp(this.x + step, 0, this.maxX);
+                        break;
                 }
 
             },
