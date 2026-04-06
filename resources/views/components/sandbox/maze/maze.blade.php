5ad348c8bdcc30399d981d140b7d5312725d8653
tomo-john <keririnkery.kery@gmail.com>
2026-04-07 07:51:59 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/maze/maze.blade.php
+++ resources/views/components/sandbox/maze/maze.blade.php
@@ -2,6 +2,49 @@
     <a href="{{ route('sandbox.page', 'index') }}" class="px-3 py-1 text-pink-500">Back</a>
 </div>
 
-<div>
-    <i class="fa-solid fa-dog text-white"></i>
+<div x-data="maze"
+     @keydown.window.prevent="handleKey($event)"
+>
+
+    <div class="w-full h-96 border rounded-lg relative">
+
+        <div class="absolute"
+             :style="{left: x + 'px', top: y + 'px'}"
+        >
+            <i class="fa-solid fa-dog text-white"></i>
+
+        </div>
+
 </div>
+
+<script>
+    function maze() {
+
+        return {
+            x: 0,
+            y: 0,
+
+            handleKey(event) {
+                console.log('押されたキーは:', event.key);
+
+                if (event.key === 'ArrowUp') {
+                    this.y -= 10;
+                }
+
+                if (event.key === 'ArrowDown') {
+                    this.y += 10;
+                }
+
+                if (event.key === 'ArrowLeft') {
+                    this.x -= 10;
+                }
+
+                if (event.key === 'ArrowRight') {
+                    this.x += 10;
+                }
+
+            },
+
+        }
+    }
+</script>
--- resources/views/sandbox/maze.blade.php
+++ resources/views/sandbox/maze.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.guest>
-    <div class="max-w-5xl mx-auto">
+    <div class="max-w-7xl mx-auto">
         <x-sandbox.maze.maze />
     </div>
 </x-layouts.guest>
