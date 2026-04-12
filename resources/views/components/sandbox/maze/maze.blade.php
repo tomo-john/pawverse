6d08d08a6425740d106953830e64ebdef17eaced
tomo-john <keririnkery.kery@gmail.com>
2026-04-12 09:33:06 +0900

    🐶 chore: sandbox

--- resources/views/components/sandbox/maze/maze.blade.php
+++ resources/views/components/sandbox/maze/maze.blade.php
@@ -18,8 +18,6 @@
             height: (config.rows * config.gridSize) + 'px'
          }"
          x-ref="field"
-         @mousemove="mousePos($event)"
-
     >
         {{-- Dog(移動判定用の枠) --}}
         <div class="absolute z-20 flex justify-center items-center transition-all"
@@ -73,10 +71,12 @@
 
         {{-- Monsters --}}
         <template x-for="monster in monsters">
-            <div class="absolute z-10 w-[50px] h-[50px] flex justify-center items-center transition-all duration-500"
+            <div class="absolute z-10 flex justify-center items-center transition-all duration-500"
                  :style="{
-                    left: monster.x + 'px',
-                    top: monster.y + 'px',
+                    left: (monster.x * config.gridSize) + 'px',
+                    top: (monster.y * config.gridSize) + 'px',
+                    width: config.gridSize + 'px',
+                    height: config.gridSize + 'px',
                  }"
             >
                 <i class="fa-solid fa-ghost text-2xl text-pink-500 animate-pulse"></i>
@@ -105,19 +105,12 @@
                 dashStep: 20,   // ダッシュ時の移動距離
             },
 
-            player: { x: 0, y: 0 },
-
             hp: 0,
-            x: 0,
-            y: 0,
-            maxX: 0,
-            maxY: 0,
             isLeft: false,
             isDash: false,
             isResetting: false,
 
-            mouseX: 0,
-            mouseY: 0,
+            player: { x: 0, y: 0 },
 
             walls: [
                 { x: 1, y: 1 },
@@ -131,14 +124,11 @@
             ],
 
             monsters: [
-                {x: 0, y: 200, dx: 10, dy: 0},
-                {x: 350, y: 150, dx: 0, dy: 10},
+                {x: 2, y: 2, dx: 1, dy: 0},
             ],
 
             init() {
                 this.hp = this.config.maxHp;
-                this.maxX = this.$refs.field.clientWidth - this.size;
-                this.maxY = this.$refs.field.clientHeight - this.size;
                 setInterval(() => {
                     this.moveMonsters();
                 }, 500);
@@ -147,7 +137,6 @@
             handleKey(event) {
                 if (this.isResetting) return;
 
-                const step = event.shiftKey ? this.config.dashStep : this.config.step;
                 let nextX = this.player.x;
                 let nextY = this.player.y;
 
@@ -172,8 +161,8 @@
                 const isInsideField =
                     gx >= 0 &&
                     gy >= 0 &&
-                    gx <= this.config.cols &&
-                    gy <= this.config.rows;
+                    gx < this.config.cols &&
+                    gy < this.config.rows;
 
                 if (!isInsideField) return false;
                     　
@@ -184,7 +173,7 @@
 
             checkGet() {
                 this.bones.forEach(bone => {
-                    if (!bone.isGet && this.isColliding(this, bone)) {
+                    if (!bone.isGet && this.isColliding(this.player, bone)) {
                         bone.isGet = true;
                         console.log('骨ゲットだわん🐶');
                     }
@@ -198,12 +187,12 @@
             moveMonsters() {
                 this.monsters.forEach(monster => {
 
-                    const newX = monster.x + monster.dx;
-                    const newY = monster.y + monster.dy;
+                    const nextX = monster.x + monster.dx;
+                    const nextY = monster.y + monster.dy;
 
-                    if (this.canMove(newX, newY)) {
-                        monster.x = newX;
-                        monster.y = newY;
+                    if (this.canMove(nextX, nextY)) {
+                        monster.x = nextX;
+                        monster.y = nextY;
                     } else {
                         monster.dx *= -1;
                         monster.dy *= -1;
@@ -217,18 +206,18 @@
                 if (this.isResetting) return;
 
                 this.monsters.forEach(monster => {
-                    if (this.isColliding(this, monster)) {
+                    if (this.isColliding(this.player, monster)) {
                         console.log('ぎゃふん🐶');
                         this.hp--;
                         this.reset();
                     }
-
-                    if (this.hp <= 0) {
-                        console.log('GAME OVER🐶');
-                        this.hp = this.config.maxHp;
-                        // ここで後で特別な処理を書く
-                    }
                 });
+
+                if (this.hp <= 0) {
+                    console.log('GAME OVER🐶');
+                    this.hp = this.config.maxHp;
+                    // ここで後で特別な処理を書く
+                }
             },
 
             isColliding(objA, objB) {
@@ -238,15 +227,15 @@
             reset() {
                 this.isResetting = true;
 
-                this.y = -50;
+                this.player.y = -1;
 
                 setTimeout(() => {
-                    this.x = 0;
+                    this.player.x = 0;
                     this.isLeft = false;
                 }, 1000);
 
                 setTimeout(() => {
-                    this.y = 0;
+                    this.player.y = 0;
                 }, 2000);
 
                 setTimeout(() => {
@@ -255,11 +244,6 @@
                 }, 3000);
             },
 
-            // デバッグ用
-            mousePos(event) {
-                this.mouseX = event.offsetX;
-                this.mouseY = event.offsetY;
-            },
         }
     }
 </script>
