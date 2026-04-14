b5733ac832feb9ddb64b12912ef67428c6035006
tomo-john <keririnkery.kery@gmail.com>
2026-04-15 06:56:29 +0900

    🐶 chore: gridSize方式で配置(テスト)

--- resources/views/livewire/dashboard/partials/no-dog.blade.php
+++ resources/views/livewire/dashboard/partials/no-dog.blade.php
@@ -1,7 +1,38 @@
-<div class="max-w-xl mx-auto text-gray-300">
-    <i class="fa-solid fa-bowl-food"></i>
-    <i class="fa-solid fa-bone"></i>
-    <span class="">まだ誰もいないわん...</span>
-    <i class="fa-solid fa-baseball"></i>
-    <i class="fa-solid fa-house"></i>
+<div class="max-w-3xl mx-auto" x-data="noDog()">
+
+    <div class="relative border rounded-lg"
+         :style="{
+            width: (cols * gridSize) + 'px',
+            height: (rows * gridSize) + 'px'
+         }"
+    >
+
+        <template x-for="item in items">
+            <div class="absolute flex justify-center items-center"
+                 :style="{
+                    left: (item.x * gridSize) + 'px',
+                    top: (item.y * gridSize) + 'px',
+                    width: gridSize + 'px',
+                    height: gridSize + 'px',
+                 }"
+            >
+                <i class="fa-solid fa-bone"></i>
+            </div>
+        </template>
+    </div>
 </div>
+
+<script>
+    function noDog() {
+
+        return {
+            gridSize: 50,
+            cols: 10,
+            rows: 6,
+            items: [
+                { x: 2, y: 2 },
+                { x: 3, y: 3 },
+            ],
+        }
+    }
+</script>
