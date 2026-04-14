7b169f3ee5c91662e9d7d767c8f6aee6635e7cb4
tomo-john <keririnkery.kery@gmail.com>
2026-04-15 07:57:57 +0900

    🐶 chore: 犬がいない世界模索中

--- resources/views/livewire/dashboard/partials/no-dog.blade.php
+++ resources/views/livewire/dashboard/partials/no-dog.blade.php
@@ -7,6 +7,7 @@
          }"
     >
 
+        {{-- 散らばったおもちゃ --}}
         <template x-for="(item, index) in items":key="index">
             <div class="absolute flex justify-center items-center"
                  :style="{
@@ -16,12 +17,27 @@
                     height: gridSize + 'px',
                  }"
             >
-                <i :class="item.icon" class="text-pink-400 text-xl"></i>
+                <i :class="item.icon"
+                    class="text-gray-300 text-xl animate-pulse transition-all duration-1000"
+                ></i>
             </div>
         </template>
 
+        {{-- メッセージ --}}
         <div class="absolute inset-0 flex justify-center items-center pointer-events-none">
             <span class="text-gray-400">まだ誰もいないワン...</span>
+        </div>
+
+        {{-- かつて犬がいた場所 --}}
+        <div class="absolute flex justify-center items-center opacity-20"
+             :style="{
+                left: (reservedSpace.x * gridSize) + 'px',
+                top: (reservedSpace.y * gridSize) + 'px',
+                width: gridSize + 'px',
+                height: gridSize + 'px',
+             }"
+        >
+            <i class="fa-solid fa-paw text-xl text-gray-400 animate-pulse duration-300"></i>
         </div>
     </div>
 </div>
@@ -44,16 +60,29 @@
                 'fa-solid fa-house'
             ],
 
+            reservedSpace: { x: 4, y: 2 },
+
             init() {
                 this.initItems();
             },
 
             initItems() {
                 this.itemsIcon.forEach(iconClass => {
+                    let x, y, isOccupied;
+
+                    do {
+                        x = Math.floor(Math.random() * this.cols),
+                        y = Math.floor(Math.random() * this.rows)
+
+                        isOccupied = this.items.some(i => i.x === x && i.y === y)
+                                     || (x === this.reservedSpace.x && y === this.reservedSpace.y);
+
+                    } while (isOccupied);
+
                     this.items.push({
                         icon: iconClass,
-                        x: Math.floor(Math.random() * this.cols),
-                        y: Math.floor(Math.random() * this.rows)
+                        x: x,
+                        y: y
                     });
                 });
             },
