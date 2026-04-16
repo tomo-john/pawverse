684394e5d12b77f7925bd17b73f86363493fb598
tomo-john <keririnkery.kery@gmail.com>
2026-04-17 07:42:52 +0900

    🐶 chore: メッセージ表示の追加

--- resources/views/livewire/dashboard/partials/no-dog.blade.php
+++ resources/views/livewire/dashboard/partials/no-dog.blade.php
@@ -17,27 +17,34 @@
                     height: gridSize + 'px',
                  }"
             >
-                <i :class="item.icon"
-                    class="text-gray-300 text-xl animate-pulse transition-all duration-1000"
-                ></i>
+                <div x-show="!item.isCollected"
+                     x-transition.duration.500ms
+                >
+                    <i :class="item.icon"
+                       @mouseenter="showMessage()"
+                       @mouseleave="hideMessage()"
+                       @click="cleanItem(index)"
+                       class="text-gray-300 text-xl animate-pulse transition-all duration-1000 cursor-pointer"
+                    ></i>
+                </div>
             </div>
         </template>
 
         {{-- メッセージ --}}
         <div class="absolute inset-0 flex justify-center items-center pointer-events-none">
-            <span class="text-gray-400">まだ誰もいないワン...</span>
+            <span class="text-gray-400"><i class="fa-solid fa-paw mx-1"></i>まだ誰もいないワン...</span>
         </div>
 
-        {{-- かつて犬がいた場所 --}}
-        <div class="absolute flex justify-center items-center opacity-20"
-             :style="{
-                left: (reservedSpace.x * gridSize) + 'px',
-                top: (reservedSpace.y * gridSize) + 'px',
-                width: gridSize + 'px',
-                height: gridSize + 'px',
-             }"
-        >
-            <i class="fa-solid fa-paw text-xl text-gray-400 animate-pulse duration-300"></i>
+    </div>
+
+    {{-- Message Window --}}
+    <div class="w-full bg-gray-100/80 border-4 border-gray-400 p-4 rounded-lg shadow-inner mt-6"
+         x-show="message"
+         x-transition.opacity.duration.500ms
+    >
+        <div class="flex items-center gap-3">
+            <i class="fa-solid fa-circle-question text-gray-400 animate-pulse"></i>
+            <p x-text="message" class="text-gray-500 font-bold text-sm"></p>
         </div>
     </div>
 </div>
@@ -60,7 +67,7 @@
                 'fa-solid fa-house'
             ],
 
-            reservedSpace: { x: 4, y: 2 },
+            message: '',
 
             init() {
                 this.initItems();
@@ -74,17 +81,29 @@
                         x = Math.floor(Math.random() * this.cols),
                         y = Math.floor(Math.random() * this.rows)
 
-                        isOccupied = this.items.some(i => i.x === x && i.y === y)
-                                     || (x === this.reservedSpace.x && y === this.reservedSpace.y);
+                        isOccupied = this.items.some(i => i.x === x && i.y === y);
 
                     } while (isOccupied);
 
                     this.items.push({
                         icon: iconClass,
                         x: x,
-                        y: y
+                        y: y,
+                        isCollected: false
                     });
                 });
+            },
+
+            showMessage() {
+                this.message = 'おもちゃをかたずけるわん...?';
+            },
+
+            hideMessage() {
+                this.message = '';
+            },
+
+            cleanItem(index) {
+                console.log('わうん🐶' + index);
             },
         }
     }
