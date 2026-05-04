34fe2b54c371cc6a38f585feec67e3f57d3abfd0
tomo-john <keririnkery.kery@gmail.com>
2026-05-04 16:53:44 +0900

    🐰 style: Index模索中

--- resources/views/livewire/dog/index.blade.php
+++ resources/views/livewire/dog/index.blade.php
@@ -1,7 +1,10 @@
-<div class="max-w-5xl mx-auto space-y-4">
+<div class="max-w-5xl mx-auto space-y-10 relative">
 
-    {{-- Toast --}}
-    <x-dog.toast />
+    {{-- Background --}}
+    <div class="absolute inset-0 pointer-events-none opacity-[0.04]">
+        <i class="fa-solid fa-paw absolute top-10 left-10 text-8xl rotate-12"></i>
+        <i class="fa-solid fa-paw absolute bottom-10 right-10 text-9xl -rotate-12"></i>
+    </div>
 
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center">
 
@@ -34,8 +37,17 @@
     </div>
 
     <flux:separator variant="subtle" class="my-6" />
+    <div class="flex items-center gap-4 py-2">
+        <div class="h-px flex-1 bg-pink-100"></div>
+        <i class="fa-solid fa-paw text-pink-300"></i>
+        <div class="h-px flex-1 bg-pink-100"></div>
+        <i class="fa-solid fa-paw text-pink-300"></i>
+    </div>
 
     {{-- Index --}}
     @include('livewire.dog.index._list')
+
+    {{-- Toast --}}
+    <x-dog.toast />
 
 </div>
