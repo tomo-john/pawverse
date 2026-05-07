589ead9cf51df446b8d7b5bd16916a009046b3b5
tomo-john <keririnkery.kery@gmail.com>
2026-05-08 07:29:04 +0900

    🐰 style: edit追加

--- resources/views/livewire/dog/index/_dog_house.blade.php
+++ resources/views/livewire/dog/index/_dog_house.blade.php
@@ -1,8 +1,9 @@
 <div wire:key="dog-{{ $dog->id }}"
-     class="relative flex flex-col items-center justify-end w-hull h-64 p-4
+     class="relative group flex flex-col items-center justify-end w-full h-64 p-4
             bg-gradient-to-b from-pink-50 to-pink-100/50 shadow-md hover:shadow-xl
             border border-pink-100 rounded-3xl
             transition-all duration-300 hover:-translate-y-1"
+     x-data="dogHouse()"
 >
 
     {{-- 地面 --}}
@@ -24,11 +25,36 @@
     </a>
 
     {{-- 犬 --}}
-    <div class="relative z-20 flex flex-col items-center">
+    <div class="relative z-20 flex flex-col items-center w-full">
         <i class="fa-solid fa-dog {{ $dog->size_class }}
                   transition-all duration-300
                   hover:scale-110 hover:-rotate-3"
            style="color: {{ $dog->color }}"></i>
+
+        <div x-show="showMessage"
+             x-transition.duration.200ms
+             class="absolute -top-10 right-0"
+        >
+            <span x-text="message" class="text-sm font-bold text-slate-500 bg-white px-2 py-1 rounded-xl"></span>
+        </div>
+    </div>
+
+    <div class="absolute bottom-5 right-8 z-30 opacity-0 group-hover:opacity-100 transition">
+        <button wire:click="edit({{ $dog->id }})"
+                class="fa-solid fa-bone text-2xl text-gray-500 -rotate-12 cursor-pointer"
+                @mouseover="showMessage = true"
+                @mouseleave="showMessage = false"
+        >
+        </button>
     </div>
 
 </div>
+
+<script>
+    function dogHouse() {
+        return {
+            showMessage: false,
+            message: 'お色直しするわん？',
+        }
+    }
+</script>
