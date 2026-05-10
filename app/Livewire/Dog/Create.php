9c216a360fd7ff235712f95e0da197b3d1fa97aa
tomo-john <keririnkery.kery@gmail.com>
2026-05-11 08:52:16 +0900

    🐶 feat: Create模索中

--- app/Livewire/Dog/Create.php
+++ app/Livewire/Dog/Create.php
@@ -9,6 +9,81 @@ use App\Models\Dog;
 
 class Create extends Component
 {
+    public $name ='';
+    public $color = '#cccccc';
+    public $size_level = null;
+    public $is_public = null;
+    public int $step = 0;
+
+    public function mount() :void
+    {
+        // mount
+    }
+
+    public function nextStep(): void
+    {
+        $this->step++;
+    }
+
+    public function prevStep(): void
+    {
+        $this->step--;
+    }
+
+    #[Computed]
+    public function currentStep(): int
+    {
+        if (!$this->hasCustomName)      return 1;
+        if (!$this->hasCustomColor)     return 2;
+        if (!$this->hasCustomSize)      return 3;
+        if (!$this->hasCustomIsPublic)  return 4;
+
+        return 5;
+    }
+
+    #[Computed]
+    public function hasCustomName(): bool
+    {
+        return filled($this->name);
+    }
+
+    #[Computed]
+    public function hasCustomColor(): bool
+    {
+        return $this->color !== '#cccccc';
+    }
+
+    #[Computed]
+    public function hasCustomSize(): bool
+    {
+        return !is_null($this-size_level);
+    }
+
+    #[Computed]
+    public function hasCustomIsPublic(): bool
+    {
+        return !is_null($this-is_public);
+    }
+
+    #[Computed]
+    public function consoleMessage(): string
+    {
+        return match($this->step) {
+            0 => 'あの子のこと教えてほしいわん',
+            1 => '名前を教えてほしいわん',
+            2 => '毛色を教えてほしいわん',
+            3 => '大きさを教えてほしいわん',
+            4 => '公開するかを教えてほしいわん',
+            default => '🐶',
+        };
+    }
+
+    #[Computed]
+    public function canSave(): bool
+    {
+        return $this->currentStep >= 5;
+    }
+
     public function render()
     {
         return view('livewire.dog.create')
--- resources/views/livewire/dog/create.blade.php
+++ resources/views/livewire/dog/create.blade.php
@@ -1,3 +1,60 @@
-<div>
-    <i class="fa-solid fa-dog text-4xl text-pink-400"></i>
+<div x-data="createDog()" class="max-w-3xl mx-auto flex flex-col gap-6">
+
+    {{-- Preview --}}
+    <div class="relative flex flex-col items-center gap-2 h-84 border">
+        <i class="fa-solid fa-house text-9xl text-pink-400"></i>
+        <i class="fa-solid fa-dog text-9xl text-pink-400"></i>
+    </div>
+
+    {{-- Console Message --}}
+    <div class="w-full bg-gray-100/80 border-4 border-gray-400 p-4 rounded-lg shadow-inner mt-6"
+         x-transition.opacity.duration.500ms
+    >
+        <div class="flex items-center gap-3 animate-pulse">
+            <i class="fa-solid fa-circle-question text-gray-400"></i>
+            <p class="text-gray-500 font-bold text-sm">{{ $this->consoleMessage }}</p>
+        </div>
+    </div>
+
+    {{-- Form --}}
+    @if($step === 1)
+        <div class="max-w-full mx-auto">
+            <input wire:model.live="name"
+                   class="w-full px-2 py-2 rounded-2xl bg-pink-50/50 border-2 border-pink-100 focus:border-pink-400 focus:ring-2 focus:ring-pink-200 outline-none transition"
+            >
+        </div>
+    @endif
+
+    {{-- 進行ボタン --}}
+    <div class="flex items-center justify-center gap-4 text-2xl">
+        @if($step === 0)
+            <button wire:click="nextStep" class="cursor-pointer text-gray-400 flex items-center gap-2 hover:animate-pulse">
+                <span class="text-sm">はじめる</span>
+            </button>
+        @endif
+
+        @if($step >= 2)
+            <button wire:click="prevStep" class="cursor-pointer"><i class="fa-regular fa-circle-left"></i></button>
+        @endif
+
+        @if($step >= 1)
+            <button wire:click="nextStep" class="cursor-pointer"><i class="fa-regular fa-circle-right"></i></button>
+        @endif
+    </div>
+
+    {{-- Debug --}}
+    <div class="flex flex-col gap-2  text-sm bg-slate-200 text-slate-500 rounded-2xl mt-10 p-2">
+        <p class="text-pink-400 font-bold">Debug Area</p>
+        <p>step: {{ $step }}</p>
+        <p>{{ $name ? $name : '名無しのわんこ' }}</p>
+    </div>
+
 </div>
+
+<script>
+    function creatDog() {
+        return {
+
+        }
+    }
+</script>
